870f78e6e4a0f9a8cde68a0f309d035d7635e54e
Miguel Briones <migg.briones@gmail.com>
2020-05-02 02:18:32 -0500

    Ejemplo de Clases y Objetos, se agrega gitignore para discriminar archivos generados por Eclipse.

---
+++ POO/src/main/Jaguar.java
@@ -0,0 +1,27 @@
+package main;
+
+public class Jaguar {
+	
+	private int edad;
+	private float peso;
+	
+	
+	public int getEdad() {
+		return edad;
+	}
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+	public float getPeso() {
+		return peso;
+	}
+	public void setPeso(float peso) {
+		this.peso = peso;
+	}
+	
+	public String toString() {
+		return "Mi edad es " + this.getEdad() + ", mi peso es " + this.getPeso();
+	}
+	
+
+}
---
+++ POO/src/main/Main.java
@@ -0,0 +1,17 @@
+package main;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		//Instanciamos la clase Jaguar
+		Jaguar yagua = new Jaguar();
+		
+		yagua.setEdad(10);
+		yagua.setPeso(120f);
+		
+		System.out.println(yagua);
+		
+	}
+
+}
